7a070de4e38f921bf1468e0c85242636a3be6814
tiagho <tiaghojeux@gmail.com>
2025-01-01 21:55:40 +0100

    updating hero page and adding a cta

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '480px',  // Custom screen size
+        'sm': '640px',  // Default small screen size
+        'md': '768px',  // Default medium screen size
+        'lg': '1024px', // Default large screen size
+        'xl': '1280px', // Default extra-large screen size
+        '2xl': '1536px', // Default 2x extra-large screen size
+        '3xl': '1800px', // Your custom size
+      },
       colors: {
         primary: '#00AEEF', 
         secondary: '#f5f5f5'
