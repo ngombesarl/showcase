7a41557fde9bc205dce8f6a33e810a17ee4343f0
tiagho <tiaghojeux@gmail.com>
2025-03-10 17:20:54 +0100

    updating sections (features with add of new ones, ingenierie for a new layout

--- src/app/components/comming-soon/comming-soon.component.ts
+++ src/app/components/comming-soon/comming-soon.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBriefcase, faDatabase, faFileAlt, faUserPlus, IconDefinition } from '@fortawesome/free-solid-svg-icons';
-
+import { faBriefcase, faDatabase, faFileAlt, faUserPlus, IconDefinition, 
+  faUsers, // Suivi du personnel
+  faMoneyBill, // Gestion de la paie
+  faSitemap, // Gestion des organigrammes
+  faStar, // Gestion des évaluations
+  faGraduationCap, // Gestion des formations
+  faCommentDots, // Discussion / Chat,
+} from '@fortawesome/free-solid-svg-icons';
 
 interface AppFeature {
   id: number,
@@ -21,6 +27,15 @@ export class CommingSoonComponent {
   faUserPlus = faUserPlus;
   faBriefCase = faBriefcase;
   faFileAlt = faFileAlt;
+
+  faUsers = faUsers;
+  faMoneyBill = faMoneyBill;
+  faSitemap = faSitemap;
+  faStar = faStar;
+  faGraduationCap = faGraduationCap;
+  faCommentDots = faCommentDots;
+  
+  
 
 
   features: AppFeature[] = [
@@ -49,5 +64,51 @@ export class CommingSoonComponent {
       icon: this.faFileAlt
     }
   ]
+
+  commingSoonFeatures: AppFeature[] = [
+    // ... fonctionnalités existantes
+    {
+      id: 5,
+      title: 'Suivi du personnel',
+      description:
+        'Gérez efficacement les informations de vos employés et suivez leurs performances.',
+      icon: this.faUsers,
+    },
+    {
+      id: 6,
+      title: 'Gestion de la paie',
+      description:
+        'Simplifiez la gestion de la paie avec des calculs précis et des rapports détaillés.',
+      icon: this.faMoneyBill,
+    },
+    {
+      id: 7,
+      title: 'Gestion des organigrammes',
+      description:
+        'Visualisez et organisez la structure de votre entreprise avec des organigrammes personnalisables.',
+      icon: this.faSitemap,
+    },
+    {
+      id: 8,
+      title: 'Gestion des évaluations',
+      description:
+        'Évaluez les performances de vos employés et suivez leur développement professionnel.',
+      icon: this.faStar,
+    },
+    {
+      id: 9,
+      title: 'Gestion des formations',
+      description:
+        'Planifiez et suivez les formations de vos employés pour améliorer leurs compétences.',
+      icon: this.faGraduationCap,
+    },
+    {
+      id: 10,
+      title: 'Discussion / Chat interne',
+      description:
+        'Facilitez la communication et la collaboration entre vos employés avec un chat interne.',
+      icon: this.faCommentDots,
+    },
+  ];
 
 }
