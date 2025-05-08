7d3e66350c547400a370804c132e9529b0aee345
tiagho <tiaghojeux@gmail.com>
2025-05-08 08:42:32 +0100

    updated icons and illustrations, positioning details on sirh nd hero, and features

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -10,40 +10,42 @@ import { AnimateOnScrollDirective } from "../../directives/animate-on-scroll.dir
 })
 export class FeaturesComponent {
 
+  onpousseUrl = "https://www.onpousse.com"
+
   features = [
     {
       id: 1 ,
-      img: 'icons/personnel.png',
+      img: 'icons/personnel.svg',
       alt: 'suivi du personnel',
       title: 'Suivi du personnel'
     },
     {
       id: 2 ,
-      img: 'icons/personnel.png',
+      img: 'icons/fourthArray.svg',
       alt: 'Paie',
       title: 'Gestion de la paie'
     },
     {
       id: 3 ,
-      img: 'icons/personnel.png',
+      img: 'icons/doubledSquare.svg',
       alt: 'Organigrammes',
       title: 'Gestion des organigrammes'
     },
     {
       id: 4 ,
-      img: 'icons/personnel.png',
+      img: 'icons/doubledSquare.svg',
       alt: 'Evaluations',
       title: 'Gestion des evaluations'
     },
     {
       id: 5 ,
-      img: 'icons/personnel.png',
+      img: 'icons/doubledSquare.svg',
       alt: 'Formations',
       title: 'Gestion des formations'
     },
     {
       id: 6 ,
-      img: 'icons/personnel.png',
+      img: 'icons/personnel.svg',
       alt: 'Chat',
       title: 'Discussion / chat interne'
     }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AnimateOnScrollDirective } from "../../directives/animate-on-scroll.directive";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,AnimateOnScrollDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -16,12 +17,12 @@ export class HeaderComponent {
   }
 
   links = [
-    {anchor: 'home',name: 'Acceuil'},
-    {anchor: 'ingenierie',name: 'Ingénierie'},
-    {anchor: 'produits',name: 'Produits'},
-    {anchor: 'services',name: 'Services'},
-    {anchor: 'partners',name: 'Partenaires'},
-    {anchor: 'about-us',name: 'A propos'},
-    {anchor: 'contact',name: 'Contact'},
+    {id: 1, anchor: 'home',name: 'Acceuil'},
+    {id: 2, anchor: 'ingenierie',name: 'Ingénierie'},
+    {id: 3, anchor: 'produits',name: 'Produits'},
+    {id: 4, anchor: 'services',name: 'Services'},
+    {id: 5, anchor: 'partners',name: 'Partenaires'},
+    {id: 6, anchor: 'about-us',name: 'A propos'},
+    {id: 7, anchor: 'contact',name: 'Contact'},
   ]
 }
